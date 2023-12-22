724e9d410a85fe3e640ad69af9a49ea050664528
Ollenotna <118982647+Ollenotna@users.noreply.github.com>
2023-12-22 01:00:39 +0100

    adios gigi

--- pages/Country_recommender.py
+++ pages/Country_recommender.py
@@ -7,7 +7,7 @@ from src.data.Data import load_data
 
 
 # Load training data:
-df = load_data('GIGI/Data/web/Train.pkl')
+df = load_data('Data/web/Train.pkl')
 beerstyles = df['1st_Style'].unique()
 country = None
 
